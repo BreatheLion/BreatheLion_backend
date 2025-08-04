bb965b2042e5274d6ae5ba1d86930f483bcd7e22
youngroky <youngrok886@gmail.com>
2025-08-04 14:34:49 +0900

    feat: init 프로젝트

---
+++ src/main/java/YAMSABU/BreatheLion_backend/BreatheLionBackendApplication.java
@@ -0,0 +1,13 @@
+package YAMSABU.BreatheLion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreatheLionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreatheLionBackendApplication.class, args);
+	}
+
+}
