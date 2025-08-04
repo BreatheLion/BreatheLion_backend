17bc112b03bc00c5cddcc4ba73418c9cac369f50
youngroky <youngrok886@gmail.com>
2025-08-04 14:34:49 +0900

    feat: init 프로젝트

---
+++ src/test/java/YAMSABU/BreatheLion_backend/BreatheLionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package YAMSABU.BreatheLion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreatheLionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
