e913118019c378f9c9138430e2d087956cfb6f73
youngroky <youngrok886@gmail.com>
2025-08-08 02:37:42 +0900

    feat: 다대다 연관 테이블작성

--- src/main/java/YAMSABU/BreatheLion_backend/organization/Organization.java
+++ src/main/java/YAMSABU/BreatheLion_backend/organization/Organization.java
@@ -1,16 +1,21 @@
 package YAMSABU.BreatheLion_backend.organization;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -38,4 +43,7 @@ public class Organization {
 
     @Column(columnDefinition = "TEXT")
     private String description;
+
+    @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DrawerOrganization> drawerOrganizations = new ArrayList<>();
 }
