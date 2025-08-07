7ff9439a925bc85228c007050e1f33b5dd81daae
youngroky <youngrok886@gmail.com>
2025-08-07 18:19:22 +0900

    feat: organ Entity

---
+++ src/main/java/YAMSABU/BreatheLion_backend/organization/Organization.java
@@ -0,0 +1,41 @@
+package YAMSABU.BreatheLion_backend.organization;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "organizations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String address;
+
+    private String url;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
