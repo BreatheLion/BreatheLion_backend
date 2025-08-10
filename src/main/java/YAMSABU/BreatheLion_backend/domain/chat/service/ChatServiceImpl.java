3ec9d0bcb16539311082fa55d6ebc1339a233b82
youngroky <youngrok886@gmail.com>
2025-08-11 01:15:02 +0900

    feat: AI 의존성 ㅎㅎ

--- src/main/java/YAMSABU/BreatheLion_backend/domain/chat/service/ChatServiceImpl.java
+++ src/main/java/YAMSABU/BreatheLion_backend/domain/chat/service/ChatServiceImpl.java
@@ -10,10 +10,13 @@ import YAMSABU.BreatheLion_backend.domain.record.repository.RecordRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.api.common.OpenAiApiClientErrorException;
 import org.springframework.stereotype.Service;
 
 import YAMSABU.BreatheLion_backend.domain.chat.dto.ChatDTO.ChatStartResponseDTO;
 import YAMSABU.BreatheLion_backend.domain.chat.dto.ChatDTO.ChatStartRequestDTO;
+
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +25,14 @@ public class ChatServiceImpl implements ChatService{
     private final ChatRepository chatRepository;
     private final SessionRepository sessionRepository;
     private final RecordRepository recordRepository;
+
+    private final ChatClient chatClient;
+
+    public String generate(String text){
+
+    }
+
+
 
     @Override
     @Transactional
