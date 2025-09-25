75e34c7013fa77257b87a86b14d5883ebf1d16be
youngroky <youngrok886@gmail.com>
2025-09-25 15:36:38 +0900

    refact: start

--- src/main/java/YAMSABU/BreatheLion_backend/global/code/SuccessCode.java
+++ src/main/java/YAMSABU/BreatheLion_backend/global/code/SuccessCode.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
     _OK(HttpStatus.OK, "200", "OK");
 
-
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
