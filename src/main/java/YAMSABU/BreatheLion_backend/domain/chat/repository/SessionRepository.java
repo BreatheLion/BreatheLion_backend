2f0bfd4906ce2eed3039e9605942f62b0eded75a
youngroky <youngrok886@gmail.com>
2025-09-25 21:38:16 +0900

    fix

--- src/main/java/YAMSABU/BreatheLion_backend/domain/chat/repository/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package YAMSABU.BreatheLion_backend.domain.chat.repository;
-
-import YAMSABU.BreatheLion_backend.domain.chat.entity.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session,Long> {
-}
